0c3445dc53e49cff4cc61314871c389c1f59b4e6
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-302.local>
2020-11-23 18:20:48 +0100

    wip

--- push.sh
+++ push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+VER=0.1
+docker build . -t asdlfkj31h/data-science:latest -t asdlfkj31h/data-science:$VER
 docker push asdlfkj31h/data-science:latest
+docker push asdlfkj31h/data-science:$VER
 
